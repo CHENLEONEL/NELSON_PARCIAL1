878b368f6268af37c4b48708473afb6ac8a50f78
nelson <nchenc1@miumg.edu.gt>
2024-03-02 10:44:23 -0600

    Ejercicio 2.2 realizado por -NELSON LEONEL CHEN CHOC / 0909 23 20379

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "PROGRAMA QUE MUESTRA NUMERO INCREMENTADOS DE 3 EN 3" << endl;
+    for (int i = 0; i <= 30; ++i) {
+        cout << "3 + " << i << " = " << 3 * i << endl;
+    }
+
+    cout << endl;
+
+    return 0;
+}
